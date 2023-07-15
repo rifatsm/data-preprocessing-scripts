b770fc80263f27dd2ab359ab569ad4494d1ff4d3
Rifat Sabbir Mansur <31485596+rifatsm@users.noreply.github.com>
2023-07-15 10:17:31 -0400

    Python script for anonymizing CSV file.

---
+++ anonymize-data.py
@@ -0,0 +1,63 @@
+"""
+This script reads a CSV file containing a 'userName' column and adds a new 'uniqueId' column to the file. The 'uniqueId' column contains a unique number for each unique 'userName' value in the original file. The updated CSV file is saved with a 'new_' prefix in the same directory as the original file.
+
+Functions:
+----------
+add_unique_numbers(file_name):
+    Reads a CSV file and adds a new 'uniqueId' column to the file. The 'uniqueId' column contains a unique number for each unique 'userName' value in the original file. The updated CSV file is saved with a 'new_' prefix in the same directory as the original file.
+
+    Parameters:
+    -----------
+    file_name : str
+        The name of the CSV file to read and update.
+
+    Returns:
+    --------
+    None
+"""
+import os
+import csv
+
+def add_unique_numbers(file_name):
+    script_dir = os.path.dirname(os.path.abspath(__file__))
+    file_path = os.path.join(script_dir, file_name)
+    
+    # Dictionary to store mapping of userName to unique numbers
+    user_numbers = {}
+    
+    # Prepare the new file path
+    new_file_name = 'new_' + file_name
+    new_file_path = os.path.join(script_dir, new_file_name)
+    
+    with open(file_path, 'r') as file, open(new_file_path, 'w', newline='') as new_file:
+        reader = csv.reader(file)
+        writer = csv.writer(new_file)
+        header = next(reader)  # Read the header row
+        
+        # Find the index of the 'userName' column
+        user_name_index = header.index('userName')
+        
+        # Remove the 'userName' column from the header
+        header.pop(user_name_index)
+        
+        # Insert 'UniqueNumber' as the first column in the header
+        header.insert(0, 'uniqueId')
+        
+        # Write the updated header to the new file
+        writer.writerow(header)
+        
+        # Generate unique numbers for each unique userName and write the updated rows to the new file
+        for row in reader:
+            user_name = row.pop(user_name_index)
+            if user_name not in user_numbers:
+                user_numbers[user_name] = len(user_numbers) + 1
+            row.insert(0, str(user_numbers[user_name]))
+            
+            # Write the updated row to the new file
+            writer.writerow(row)
+            
+    print(f"New CSV file '{new_file_name}' has been created.")
+
+# Example usage
+csv_file_name = 'file_name.csv'
+add_unique_numbers(csv_file_name)
